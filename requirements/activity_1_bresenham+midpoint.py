5d4b970e0ce8b4bb82c00f53355a17f0b2961783
plnjbautista <paulinejoy.bautista@wvsu.edu.ph>
2023-03-08 06:35:25 +0800

    message

---
+++ requirements/activity_1_bresenham+midpoint.py
@@ -0,0 +1,62 @@
+#Activity 1 by Pauline Joy O. Bautista
+#Python program for Bresenham's Line.
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as buli
+plt.title('Bresenham Line Algorithm')
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+
+def bres(x1, y1, x2, y2, color):
+    x,y = x1,y1
+    dx = abs(x2 - x1)
+    dy = abs(y2 -y1)
+    a = dy/float(dx)
+
+    if a > 1:
+        dx, dy = dy, dx
+        x, y = y, x
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+
+    p = 2*dy - dx
+    
+    # Initialize the plotting points
+    xcoordinates = [x]
+    ycoordinates = [y]
+    
+    #midpoint
+    mx = (x2+x1)/2
+    my = (y2+y1)/2
+    print ('Midpoint is:', mx, ',', my)
+    buli.plot (mx, my, marker="o", markerfacecolor='blue')
+         
+    #calculate increment in x & y for each steps
+    for i in range(2, dx + 2):
+        if p > 0:
+            y = y + 1 if y < y2 else y - 1
+            p = p + 2 * (dy - dx)
+        else:
+            p = p + 2 * dy
+
+        x = x + 1 if x < x2 else x - 1
+        
+        xcoordinates.append(x)
+        ycoordinates.append(y)
+
+    plt.plot(xcoordinates, ycoordinates)
+    plt.show()
+
+def main():
+   x = int(input("Enter X1: "))
+   y = int(input("Enter Y1: "))
+   xEnd = int(input("Enter X2: "))
+   yEnd = int(input("Enter Y2: "))
+   color = "y."
+   bres(x, y, xEnd, yEnd, color)
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
